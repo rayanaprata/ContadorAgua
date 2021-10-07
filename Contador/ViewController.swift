0326a180d3581edc212db0fb80bcd0087970f3dd
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-07 15:44:17 -0300

    ajustando interface do usuário

--- Contador/ViewController.swift
+++ Contador/ViewController.swift
@@ -5,15 +5,59 @@
 //  Created by Rayana Prata Neves on 07/10/21.
 //
 
+ /*
+    - Label -> UILabel
+    - Botão -> UIButton
+    - Imagem -> UIImage ou UIImageView
+    - Storyboard
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    var contadorCoposDagua: Int = 0
+    
+    // MARK: Outlets
+    @IBOutlet weak var labelQtdAgua: UILabel!
+    @IBOutlet weak var buttonAumentar: UIButton!
+    @IBOutlet weak var buttonDiminuir: UIButton!
+    @IBOutlet weak var labelQuantidadeTotal: UILabel!
+    @IBOutlet weak var imageViewCopoDagua: UIImageView!
+    @IBOutlet weak var viewQt: UIView!
+    
+    // MARK: Initialization
+    
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
-
+    
+    // MARK: Actions
+    @IBAction func incrementaContador(_ sender: Any) {
+        contadorCoposDagua += 100
+        labelQuantidadeTotal.text = String(contadorCoposDagua) + "ml"
+    }
+    
+    @IBAction func decrementaContador(_ sender: Any) {
+        
+        if contadorCoposDagua != 0 {
+            contadorCoposDagua -= 100
+            labelQuantidadeTotal.text = String(contadorCoposDagua) + "ml"
+        }
+    }
+    
+    // MARK: Methods
+    private func setupUI() {
+        buttonAumentar.layer.cornerRadius = 10
+        buttonDiminuir.layer.cornerRadius = 10
+        labelQuantidadeTotal.text = ""
+        imageViewCopoDagua.image = UIImage(named: "agua")
+        viewQt.layer.cornerRadius = 10
+    }
 
 }
 
